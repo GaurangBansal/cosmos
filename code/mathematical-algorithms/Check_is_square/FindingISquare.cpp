0fa60f384e4cc07b3f1652cedea73fed548dda05
GaurangBansal <44459969+GaurangBansal@users.noreply.github.com>
2019-01-12 17:02:11 +0530

    update filesquare.cpp
    the implementation before has implementation error

--- code/mathematical-algorithms/Check_is_square/FindingISquare.cpp
+++ code/mathematical-algorithms/Check_is_square/FindingISquare.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-	int t;
-	cout<<"Enter no. of test cases"<<endl;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cout<<"Enter the number"<<endl;
-		cin>>n;
-		int i;
-		for(i=1;i<=n;i++)
-		{
-			if(i*i==n)
-			{
-			
-			cout<<"Yes";
-			return 0;
-		}
-		}
-	  cout<<"No";
-		return 0;
-		
-	}
+    int t;// test cases
+    cout<<"type number of test cases :"<<endl;
+    cin>>t;
+    while(t--)
+        {
+        int n,i;// n= number
+        int temp=0;
+        cout<<"Enter number :"<<endl;
+        cin>>n;
+        for(i=0;i<=n;i++)
+        {
+            if(i*i==n)
+            {
+                temp=1;
+                break;
+            }
+
+        }
+        if(temp==1)
+            cout<<"yes and number is :"<<i<<endl;
+        else
+            cout<<"not found"<<endl;
+
+    }
+
+    return 0;
+
 }
